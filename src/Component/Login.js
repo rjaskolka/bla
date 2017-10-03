5112b9ff44d605ad987a8a944540f424b618217c
Rafal Jaskolka <rjaskolka@gmail.com>
2017-10-03 16:26:45 +0200

    switch login

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -6,18 +6,35 @@ export default class Login extends Component {
   constructor (props) {
     super(props)
       this.state = {
-        loggedIn: false
+        loggedIn: false,
+        buttonText: 'Press to login'
       }
   }
   componentWillMount() {
     console.log(this.state.loggedIn)
   }
 
+  buttonPressed = () => {
+    let currentState = this.state.loggedIn
+    console.log('button pressed')
+    if(currentState) {
+      this.setState({
+        loggedIn: !currentState,
+        buttonText: 'Press to logout'
+      })
+    } else {
+      this.setState({
+        loggedIn: !currentState,
+        buttonText: 'Press to login'
+      })
+    }
+  }
+
   render() {
     return (
       <View style= { styles.container } >
-      <TouchableHighlight style= { styles.button } >
-          <Text style= { styles.buttonText } >Press to login</Text>
+      <TouchableHighlight style= { styles.button } onPress={this.buttonPressed}>
+          <Text style= { styles.buttonText } >{ this.state.buttonText}</Text>
 	</TouchableHighlight>
       </View>
     );
